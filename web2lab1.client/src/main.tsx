147d8b6c20411d6ddd336c24cc66a80064096334
Igor Sostarko <igor.sostarko@fer.hr>
2024-10-25 20:46:20 +0200

    updated main.tsx

--- web2lab1.client/src/main.tsx
+++ web2lab1.client/src/main.tsx
@@ -5,13 +5,19 @@ import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import TicketInfo from './Pages/TicketInfo.tsx'
+import { useNavigate } from "react-router-dom"
 
 const domain="dev-sx10l5srw3t5xwff.us.auth0.com"
 const clientId="FwK0bpM82iAZTgkZeRZQx0koeuXTPELF"
 
+const onRedirectCallback = (appState : any) => {
+  const navigate = useNavigate()
+  navigate(appState?.returnTo || window.location.pathname);
+}
+
 createRoot(document.getElementById('root')!).render(
   <Auth0Provider domain={domain} clientId={clientId}
-    authorizationParams={{ redirect_uri: window.location.origin, response_type: 'code', audience: "https://ticketapi" }}>
+    authorizationParams={{ redirect_uri: window.location.origin, response_type: 'code', audience: "https://ticketapi" }} onRedirectCallback={onRedirectCallback}>
     {/* <StrictMode> */}
       <BrowserRouter>
         <Routes>
